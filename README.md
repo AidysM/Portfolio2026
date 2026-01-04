75ae88bcb7dd11beed96ce7c2fa4dbab59082a17
AidysM <mongush10011979@gmail,com>
2026-01-04 22:38:39 +0300

    added templates

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ poetry self add poetry-plugin-shell
 
 poetry shell
 
+
+echo "# Portfolio2026" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/AidysM/Portfolio2026.git
+git push -u origin main
+
---
+++ home/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from .views import HomePageView, ResumePageView, ProjectsPageView, ContactPageView
+
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('resume/', ResumePageView.as_view(), name='resume'),
+    path('projects/', ProjectsPageView.as_view(), name='projects'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
+]
+
